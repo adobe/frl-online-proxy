74e2f5a08b7070b085c5b520a1d8122f8fb39184
Andrew Dorton <adorton@adobe.com>
2020-11-11 14:00:53 -0700

    node setup script

--- .build/node-setup.sh
+++ .build/node-setup.sh
@@ -1,5 +1,5 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash &&  sudo "$HOME/.nvm/nvm.sh"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
+nvm install v15
